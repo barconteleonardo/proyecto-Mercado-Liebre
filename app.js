0e0813ea2627c9a3be29aca86682545601e357e2
barconteleonardo <barconteleonardo@gmail.com>
2023-08-18 01:43:03 -0300

    Mercado Liebre

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const path = require("path");
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, './public')));
 
-app.get("/", (req, res) =>{
-    res.sendFile(path.resolve(__dirname,"./views/home.html"));
-})
+app.get('/', (_req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/home.html'));
+});
 
-app.listen(port, () =>{
-    console.log(`Example app listening on port ${port}`);
-})
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
